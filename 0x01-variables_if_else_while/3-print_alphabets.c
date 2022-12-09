74f5305aa8b5b53a063f2fc5a7672a8a9c1a8bba
Asdenaki <asdenakitamrat@gmail.com>
2022-12-09 01:09:52 -0800

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int s = 'a';
+
+	int c = 'A';
+
+	while
+		(s <= 'z') {
+			putchar(s);
+			s++;
+		}
+	while
+		(c <= 'Z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+
+}
