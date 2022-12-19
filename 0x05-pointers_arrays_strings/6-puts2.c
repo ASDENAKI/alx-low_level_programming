10c16a43f7926a82dcae5dc08eb65586cf91fd35
Asdenaki <asdenakitamrat@gmail.com>
2022-12-19 10:40:20 -0800

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: holds the string
+ */
+
+void puts2(char *str)
+{
+	int i, j;
+
+	i = strlen(str);
+
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
