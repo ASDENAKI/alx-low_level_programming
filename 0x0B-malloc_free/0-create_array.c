fc6b520c7be55ac1ec962c187b1f69ace2e69b04
Asdenaki <asdenakitamrat@gmail.com>
2023-01-11 11:23:50 -0800

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	buffer = (char*) malloc(size * sizeof(c));
+	buffer = (char *) malloc(size * sizeof(c));
 
 	if (buffer == 0)
 	{
