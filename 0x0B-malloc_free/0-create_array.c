75473fd1b103bc613b38e5f8c7a061a229435dfe
Asdenaki <asdenakitamrat@gmail.com>
2023-01-11 11:19:28 -0800

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - function that creates an array of chars, and initializes it
+ * @size: size of the array
+ * @c:char
+ * Return: pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *buffer;
+	unsigned int position;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	buffer = (char*) malloc(size * sizeof(c));
+
+	if (buffer == 0)
+	{
+		return (NULL);
+	}
+
+	else
+	{
+		position = 0;
+		while (position < size)
+		{
+			*(buffer + position) = c;
+			position++;
+		}
+		return (buffer);
+	}
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strlen - count
+ * @s: array
+ * Return: 1
+ */
+int _strlen(char *s)
+{
+	unsigned int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * str_concat - a function that concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: array
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *dest;
+	unsigned int i, j, size;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	size = (_strlen(s1) + _strlen(s2) + 1);
+
+	dest = (char *) malloc(size * sizeof(char));
+
+	if (dest == 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; *(s1 + i) != '\0'; i++)
+	{
+		*(dest + 1) = *(s1 + 1);
+	}
+	for (j = 0; *(s2 + j) != '\0'; j++)
+	{
+		*(dest + i) = *(s2 + j);
+		i++;
+	}
+	return (dest);
+}
