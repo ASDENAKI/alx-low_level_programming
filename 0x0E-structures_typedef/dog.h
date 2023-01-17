bdbfeca15ca8302c20dea2e7ea0c52a57f33e534
Asdenaki <asdenakitamrat@gmail.com>
2023-01-17 01:15:59 -0800

    first task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef dog_h
 #define dog_h
 
+dog_t *new_dog(char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 /**
  * struct dog - dog's information
  * @name: name of the dog
