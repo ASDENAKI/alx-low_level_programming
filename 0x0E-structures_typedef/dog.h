6a030fdc15f942d2687ff4654363e045d2d25759
Asdenaki <asdenakitamrat@gmail.com>
2023-01-17 01:04:29 -0800

    first task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - dog's information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ *
+ * Description: This structure holdes info about dogs
+ */
+
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+
+#endif
